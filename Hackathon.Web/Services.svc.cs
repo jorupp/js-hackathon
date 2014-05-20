0a346fd10c401fb703cdc1d1835f7ce9d553cedf
jorupp <jorupp@gmail.com>
2014-05-20 14:45:21 -0500

    Add shell of services

---
+++ Hackathon.Web/Services.svc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace Hackathon.Web
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class Services
+    {
+        // Add [WebGet] attribute to use HTTP GET
+        [OperationContract]
+        [WebGet(BodyStyle = WebMessageBodyStyle.Bare, ResponseFormat = WebMessageFormat.Json)]
+        public X[] GetData()
+        {
+            return new X[] 
+            {
+                new X { x = 1, y = 2 },
+            };
+        }
+
+        public class X
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+        }
+    }
+}
