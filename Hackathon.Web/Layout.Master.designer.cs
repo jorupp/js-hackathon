77703ce5fc15b8fd9e626f2874cf947e99260868
jorupp <jorupp@gmail.com>
2014-05-27 10:26:44 -0500

    Other half of the form removal

--- Hackathon.Web/Layout.Master.designer.cs
+++ Hackathon.Web/Layout.Master.designer.cs
@@ -23,15 +23,6 @@ namespace Hackathon.Web {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
         
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
         /// body control.
         /// </summary>
         /// <remarks>
